e645035af7eb30afb71ea9528df90d62eb829166
Corinna Brock <corinna.brock@gmail.com>
2012-07-16 11:11:46 -0400

    working more

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,2 +1,20 @@
 class AttendeesController < ApplicationController
+  def new
+    @attendee=Attendee.new
+
+      respond_to do |format|
+        format.html # new.html.erb
+        format.json { render json: @song }
+    end
+  end
+
+  def list
+    @attendee = Attendee.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @songs }
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 RSVPApp::Application.routes.draw do
+  
+
+  post "/new" => 'attendees#list'
+
   resources :attendees
+
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +56,7 @@ RSVPApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'attendees/new'
+  root :to => 'attendees#new'
 
   # See how all your routes lay out with "rake routes"
 
